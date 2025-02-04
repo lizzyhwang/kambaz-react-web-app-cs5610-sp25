1632f01de73459e269c8cc185254f16083860a80
Lizzy Hwang <hwang.eli@northeastern.edu>
2025-02-03 23:32:30 -0500

    2.4.5

--- src/kambaz/courses/home/status.tsx
+++ src/kambaz/courses/home/status.tsx
@@ -1,13 +1,35 @@
+import { MdDoNotDisturbAlt } from "react-icons/md";
+import { FaCheckCircle } from "react-icons/fa";
+import { BiBarChart, BiImport, BiSolidMegaphone } from "react-icons/bi";
+import { LiaBell, LiaDesktopSolid, LiaFileImportSolid, LiaHomeSolid } from "react-icons/lia";
+import { Button } from "react-bootstrap";
+
 export default function CourseStatus() {
   return (
-    <div id="wd-course-status">
+    <div id="wd-course-status" style={{ width: "350px" }}>
       <h2>Course Status</h2>
-      <button>Unpublish</button> <button>Publish</button>
-      <br></br><br></br>
-
-      <button>Import Existing Content</button><br></br>
-      <button>New Announcement</button><br></br>
-      <button>New Analytics</button><br></br>
-      <button>View Course Notifications</button><br></br>
+      <div className="d-flex">
+        <div className="w-50 pe-1">
+          <Button variant="secondary" size="lg" className="w-100 text-nowrap ">
+            <MdDoNotDisturbAlt className="me-2 fs-5" /> Unpublish </Button> </div>
+        <div className="w-50">
+          <Button variant="success" size="lg" className="w-100">
+            <FaCheckCircle className="me-2 fs-5" /> Publish </Button> </div>
+      </div>
+      <br />
+      <Button variant="secondary" size="lg" className="w-100 mt-1 text-start">
+        <BiImport className="me-2 fs-5" /> Import Existing Content </Button>
+      <Button variant="secondary" size="lg" className="w-100 mt-1 text-start">
+        <LiaFileImportSolid className="me-2 fs-5" /> Import from Commons </Button>
+      <Button variant="secondary" size="lg" className="w-100 mt-1 text-start">
+        <LiaHomeSolid className="me-2 fs-5" /> Choose Home Page </Button>
+      <Button variant="secondary" size="lg" className="w-100 mt-1 text-start">
+        <LiaDesktopSolid className="me-2 fs-5" /> View Course Screen </Button>
+      <Button variant="secondary" size="lg" className="w-100 mt-1 text-start">
+        <BiSolidMegaphone className="me-2 fs-5" /> New Announcement </Button>
+      <Button variant="secondary" size="lg" className="w-100 mt-1 text-start">
+        <BiBarChart className="me-2 fs-5" /> New Analytics </Button>
+      <Button variant="secondary" size="lg" className="w-100 mt-1 text-start">
+        <LiaBell className="me-2 fs-5" /> View Course Notifications </Button>
     </div>);
 }
