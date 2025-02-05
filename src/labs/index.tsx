8fe2d66c65f68654941f00d1b272b09125e59b25
Lizzy Hwang <hwang.eli@northeastern.edu>
2025-02-05 01:30:42 -0500

    section number

--- src/labs/index.tsx
+++ src/labs/index.tsx
@@ -8,7 +8,8 @@ export default function Labs() {
   return (
     <div>
       <h1>Welcome to Web Dev</h1>
-      <h1>Elizabeth Hwang</h1>
+      <h2>Elizabeth Hwang</h2>
+      <h5>Section 01</h5>
       <TOC />
       <Routes>
         <Route path="/" element={<Navigate to="Lab1" />} />
