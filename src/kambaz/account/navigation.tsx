92ad543ba9d5db4b9a4b671c12bd078151408dc7
Lizzy Hwang <hwang.eli@northeastern.edu>
2025-03-11 01:08:42 -0400

    a4 4.4.4 complete

--- src/kambaz/account/navigation.tsx
+++ src/kambaz/account/navigation.tsx
@@ -1,12 +1,14 @@
 import { ListGroup } from "react-bootstrap";
 import { Link, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 export default function AccountNavigation() {
   const { pathname } = useLocation();
-  const links = [
-    { label: "Signin", path: `/Kambaz/Account/Signin` },
-    { label: "Signup", path: `/Kambaz/Account/Signup` },
-    { label: "Profile", path: `/Kambaz/Account/Profile` }
-  ];
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const links = !currentUser ?
+    [{ label: "Signin", path: `/Kambaz/Account/Signin` }, { label: "Signup", path: `/Kambaz/Account/Signup` }]
+    :
+    [{ label: "Profile", path: `/Kambaz/Account/Profile` }];
 
   return (
     <ListGroup id="wd-account-navigation" className="wd list-group fs-7 rounded-0">
--- src/kambaz/account/profile.tsx
+++ src/kambaz/account/profile.tsx
@@ -1,58 +1,51 @@
-import { FormControl, FormSelect, InputGroup } from "react-bootstrap";
-import { IoCalendarOutline } from "react-icons/io5";
-import { Link } from "react-router-dom";
+import { Button, FormControl } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 import { setCurrentUser } from "./reducer";
 import { useDispatch, useSelector } from "react-redux";
+
 export default function Profile() {
+  const [profile, setProfile] = useState<any>({});
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { currentUser } = useSelector((state: any) => state.accountReducer);
-  console.log(currentUser)
+  const fetchProfile = () => {
+    if (!currentUser) return navigate("/Kambaz/Account/Signin");
+    setProfile(currentUser);
+  };
+  const signout = () => {
+    dispatch(setCurrentUser(null));
+    navigate("/Kambaz/Account/Signin");
+  };
+  useEffect(() => { fetchProfile(); }, []);
+
   return (
     <div id="wd-profile-screen">
       <h1>Profile</h1>
-      <FormControl id="wd-username"
-        placeholder="Username"
-        className="mb-2"
-        value={currentUser.username} />
-      <FormControl id="wd-password"
-        placeholder="Password"
-        type="password"
-        className="mb-2"
-        value={currentUser.password} />
-      <FormControl id="wd-fname"
-        placeholder="First Name"
-        className="mb-2"
-        value={currentUser.firstName} />
-      <FormControl id="wd-lname"
-        placeholder="Last Name"
-        className="mb-2"
-        value={currentUser.lastName} />
-      <FormControl id="wd-email"
-        placeholder="email@example.com"
-        value={currentUser.email}
-        className="mb-2" />
-
-      <InputGroup className="mb-2">
-        <FormControl
-          value={currentUser.dob}
-          placeholder="DOB" />
-        <InputGroup.Text>
-          <IoCalendarOutline />
-        </InputGroup.Text>
-      </InputGroup>
-
-      <FormSelect id="wd-role" className="mb-3">
-        <option value="USER">User</option>
-        <option value="ADMIN">Admin</option>
-        <option value="FACULTY">Faculty</option>
-        <option value="Student">Student</option>
-      </FormSelect>
-
-      <Link id="wd-signin-btn"
-        to="/Kambaz/Account/Signin"
-        className="btn btn-danger w-100 mb-2"
-        onClick={() => dispatch(setCurrentUser(null))}>
-        Sign Out </Link>
+      {profile && (
+        <div>
+          <FormControl defaultValue={profile.username} id="wd-username" className="mb-2"
+            onChange={(e) => setProfile({ ...profile, username: e.target.value })} />
+          <FormControl defaultValue={profile.password} id="wd-password" className="mb-2"
+            onChange={(e) => setProfile({ ...profile, password: e.target.value })} />
+          <FormControl defaultValue={profile.firstName} id="wd-firstname" className="mb-2"
+            onChange={(e) => setProfile({ ...profile, firstName: e.target.value })} />
+          <FormControl defaultValue={profile.lastName} id="wd-lastname" className="mb-2"
+            onChange={(e) => setProfile({ ...profile, lastName: e.target.value })} />
+          <FormControl defaultValue={profile.dob} id="wd-dob" className="mb-2"
+            onChange={(e) => setProfile({ ...profile, dob: e.target.value })} type="date" />
+          <FormControl defaultValue={profile.email} id="wd-email" className="mb-2"
+            onChange={(e) => setProfile({ ...profile, email: e.target.value })} />
+          <select onChange={(e) => setProfile({ ...profile, role: e.target.value })}
+            className="form-control mb-2" id="wd-role">
+            <option value="USER">User</option>            <option value="ADMIN">Admin</option>
+            <option value="FACULTY">Faculty</option>      <option value="STUDENT">Student</option>
+          </select>
+          <Button onClick={signout} className="w-100 mb-2" id="wd-signout-btn">
+            Sign out
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
